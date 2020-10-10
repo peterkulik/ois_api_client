f9bde3eab5df82ab36ae9cf1710611c3adfba8ec
peter <peter@importas.dev>
2020-10-10 14:35:01 +0200

    Added requests to setup.py as a required package.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ois_api_client",
-    version="0.0.1",
+    version="0.0.3",
     author="Peter Kulik",
     author_email="peter@importas.dev",
     description="A python client library for the hungarian Online Invoice System API",
@@ -13,12 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/peterkulik/ois_api_client",
     packages=setuptools.find_packages(),
+    install_requires=['requests'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Office/Business",
@@ -27,5 +27,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
 
     ],
-    python_requires='>=3.6.1',
+    python_requires='>=3.7.0',
 )
