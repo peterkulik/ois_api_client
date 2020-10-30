7e67562096424121691e3a7ede8094f3f7f666de
peter <peter@importas.dev>
2020-10-30 22:18:23 +0100

    Added __init__.py files for tests packages

--- tests/common/parameter/builders/test_build_request_signature.py
+++ tests/common/parameter/builders/test_build_request_signature.py
@@ -1,10 +1,9 @@
 import ois_api_client as ois
 import pytest
 from tests.common import config
-from tests.common.config import expected_request_signature
 
 
 @pytest.mark.skip(reason='use fix timestamp')
 def test_build_request_signature_without_hash_list_success():
     rs = ois.build_request_signature(config.get_request_id(), config.get_timestamp(), config.signature_key)
-    assert rs == expected_request_signature
+    assert rs == config.expected_request_signature
