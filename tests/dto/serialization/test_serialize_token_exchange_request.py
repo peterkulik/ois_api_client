6984326a46ed813cd1f756e50efcdbe46da149eb
peter <peter@importas.dev>
2020-11-05 19:03:40 +0100

    Implemented test_serialize_token_exchange_request.py

---
+++ tests/dto/serialization/test_serialize_token_exchange_request.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+
+import pytest
+
+import ois_api_client as ois
+from ois_api_client import NAMESPACE_API
+from ois_api_client.dto.serialization.serialize_token_exchange_request import serialize_token_exchange_request
+from ois_api_client.dto.xml.get_full_tag import get_full_tag
+from tests.dto.serialization.validate_element import validate_element, validate_tag_name
+
+
+@pytest.mark.parametrize(
+    argnames=('request_id', 'timestamp', 'request_signature', 'password_hash', 'login', 'tax_number',
+              'software_id', 'software_name', 'software_operation', 'software_main_version',
+              'software_dev_name', 'software_dev_contact'),
+    argvalues=[
+        ('', datetime.now(), '', '', '', '', '', '', ois.SoftwareOperation.LOCAL_SOFTWARE, '', '', '')
+    ])
+def test_serialize_token_exchange_request(request_id: str, timestamp: datetime, request_signature: str,
+                                          password_hash: str, login: str, tax_number: str, software_id: str,
+                                          software_name: str, software_operation: ois.SoftwareOperation,
+                                          software_main_version: str, software_dev_name: str,
+                                          software_dev_contact: str):
+    data = ois.TokenExchangeRequest(
+        header=ois.BasicHeader(
+            request_id=request_id,
+            timestamp=timestamp
+        ),
+        user=ois.UserHeader(
+            login=login,
+            tax_number=tax_number
+        ),
+        software=ois.Software(
+            id=software_id,
+            name=software_name,
+            operation=software_operation,
+            main_version=software_main_version,
+            dev_name=software_dev_name,
+            dev_contact=software_dev_contact
+        )
+    )
+
+    result = serialize_token_exchange_request(data, request_signature, password_hash=password_hash)
+
+    assert get_full_tag(NAMESPACE_API, 'TokenExchangeRequest') == result.tag
+
+    children = [i for i in result]
+
+    assert 3 == len(children)
+
+    validate_tag_name(children[0], ois.NAMESPACE_COMMON, 'header')
+    validate_tag_name(children[1], ois.NAMESPACE_COMMON, 'user')
+    validate_tag_name(children[2], ois.NAMESPACE_API, 'software')
--- tests/dto/serialization/validate_element.py
+++ tests/dto/serialization/validate_element.py
@@ -3,6 +3,10 @@ import xml.etree.ElementTree as ET
 from ois_api_client.dto.xml.get_full_tag import get_full_tag
 
 
+def validate_tag_name(element: ET.Element, namespace: str, tag_name: str):
+    assert get_full_tag(namespace, tag_name) == element.tag
+
+
 def validate_element(element: ET.Element, namespace: str, tag_name: str, expected_value: str):
-    assert element.tag == get_full_tag(namespace, tag_name)
+    validate_tag_name(element, namespace, tag_name)
     assert expected_value == element.text
