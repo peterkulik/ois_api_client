29b02c1dd2881a02cb9448d3ba254caf0a9231f6
peter <peter@importas.dev>
2021-01-29 20:27:30 +0100

    Added NAMESPACE_API for serialize_query_invoice_data_request.py and serialize_query_invoice_digest_request.py

--- ois_api_client/dto/serialization/serialize_query_invoice_data_request.py
+++ ois_api_client/dto/serialization/serialize_query_invoice_data_request.py
@@ -3,19 +3,23 @@ import xml.etree.ElementTree as ET
 from .serialize_basic_online_invoice_request import serialize_basic_online_invoice_request
 from .serialize_element import serialize_text_element, serialize_int_element
 from .. import QueryInvoiceDataRequest
+from ... import NAMESPACE_API
 
 
 def serialize_query_invoice_data_request_type(parent: ET.Element, data: QueryInvoiceDataRequest) -> ET.Element:
     result = ET.SubElement(parent, 'invoiceNumberQuery')
 
-    serialize_text_element(result, 'invoiceNumber', data.invoice_number_query.invoice_number)
-    serialize_text_element(result, 'invoiceDirection', data.invoice_number_query.invoice_direction.value)
+    serialize_text_element(result, 'invoiceNumber', data.invoice_number_query.invoice_number, NAMESPACE_API
+                           )
+    serialize_text_element(result, 'invoiceDirection', data.invoice_number_query.invoice_direction.value,
+                           NAMESPACE_API)
 
     if data.invoice_number_query.batch_index is not None:
-        serialize_int_element(result, 'batchIndex', data.invoice_number_query.batch_index)
+        serialize_int_element(result, 'batchIndex', data.invoice_number_query.batch_index, NAMESPACE_API)
 
     if data.invoice_number_query.supplier_tax_number is not None:
-        serialize_text_element(result, 'supplierTaxNumber', data.invoice_number_query.supplier_tax_number)
+        serialize_text_element(result, 'supplierTaxNumber', data.invoice_number_query.supplier_tax_number,
+                               NAMESPACE_API)
 
     return result
 
--- ois_api_client/dto/serialization/serialize_query_invoice_digest_request.py
+++ ois_api_client/dto/serialization/serialize_query_invoice_digest_request.py
@@ -7,14 +7,15 @@ from .serialize_mandatory_query_params import serialize_mandatory_query_params
 from .serialize_relational_query_params import serialize_relational_query_params
 from .serialize_transaction_query_params import serialize_transaction_query_params
 from ..QueryInvoiceDigestRequest import QueryInvoiceDigestRequest
+from ... import NAMESPACE_API
 
 
 def serialize_query_invoice_digest_request(data: QueryInvoiceDigestRequest, request_signature: str,
                                            password_hash: str) -> ET.Element:
     root = serialize_basic_online_invoice_request(data, 'QueryInvoiceDigestRequest', request_signature, password_hash)
 
-    serialize_int_element(root, 'page', data.page)
-    serialize_text_element(root, 'invoiceDirection', data.invoice_direction.value)
+    serialize_int_element(root, 'page', data.page, NAMESPACE_API)
+    serialize_text_element(root, 'invoiceDirection', data.invoice_direction.value, NAMESPACE_API)
 
     query_params_element = ET.SubElement(root, 'invoiceQueryParams')
     serialize_mandatory_query_params(query_params_element, data.invoice_query_params.mandatory_query_params.parameter)
