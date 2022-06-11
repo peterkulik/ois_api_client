37c1065c34e1d4934e0746ab2c33bf02beef757d
peter <peter@importas.dev>
2022-06-11 22:52:27 +0200

    Version number update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ois_api_client",
-    version="0.3.4",
+    version="0.3.5",
     author="Peter Kulik",
     author_email="peter@importas.dev",
     description="A python client library for the hungarian Online Invoice System API",
